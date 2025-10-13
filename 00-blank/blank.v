bd2b80ec0c9f0ba21ffc9ee53ac0e11ad1a5930c
Nathan Neidigh <nathan.neidigh@outlook.com>
2025-10-12 17:35:25 -0700

    Adding a blank verilog module to clear the flash

---
+++ 00-blank/blank.v
@@ -0,0 +1,2 @@
+module blank ();
+endmodule
--- 01-Lab/blink.v
+++ 01-Lab/blink.v
@@ -1,16 +1,22 @@
 module blink (
     input  CLK,
-    output LED_R
+    output LED_R,
+    output LED_G,
+    output LED_B
 );
 
   localparam HALF_PERIOD = 500_000;
 
   reg [18:0] count = 19'd0;
   reg r_LED_R = 1'b0;
+  reg r_LED_G = 1'b0;
+  reg r_LED_B = 1'b0;
 
   always @(posedge CLK) begin
     if (count == HALF_PERIOD - 1) begin
       r_LED_R <= ~r_LED_R;
+      r_LED_G <= ~r_LED_G;
+      r_LED_B <= ~r_LED_B;
       count   <= 0;
     end else begin
       count <= count + 1;
@@ -18,5 +24,7 @@ module blink (
   end
 
   assign LED_R = r_LED_R;
+  //assign LED_G = r_LED_G;
+  //assign LED_B = r_LED_B;
 
 endmodule
